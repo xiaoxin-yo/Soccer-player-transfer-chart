314815d1f4b2a91f1db9fd29bd08fb3d8e3a1169
Yuxin Z <xiaoxin.yo@gmail.com>
2015-11-14 18:10:33 -0800

    Two "names"
    Two texts moving together

--- basemap.py
+++ basemap.py
@@ -7,10 +7,7 @@ matplotlib.rcParams['backend'] = "Qt4Agg"
 
 fig = plt.figure(1)
 # create Basemap instance for Robinson projection.
-m = Basemap(llcrnrlon=-20.,llcrnrlat=35.,urcrnrlon=60.,urcrnrlat=60.,\
-            rsphere=(6378137.00,6356752.3142),\
-            resolution='l',projection='merc',\
-            area_thresh=10000000.)
+m = Basemap(llcrnrlon=-175,llcrnrlat=-80,urcrnrlon=185,urcrnrlat=80,projection='mill')
 # draw costlines and coutries
 m.drawcoastlines(linewidth=0.5)
 m.drawcountries(linewidth=0.5)
@@ -21,11 +18,13 @@ x,y = m(0, 0)
 
 # draw a point
 point = plt.text(x,y,'Name', fontsize=20)
+point2 = plt.text(x,y,'Name2', fontsize=20)
 
 # this is called every frame
 def init():
-    #point.set_position([[],[]])
-    return point,
+    point.set_fontsize(0)
+    point2.set_fontsize(0)
+    return point,point2
 
 start_lon = -20.
 start_lat = 35.
@@ -37,12 +36,16 @@ slope = (end_lat - start_lat) / (end_lon - start_lon)
 
 # animation function.  This is called sequentially
 def animate(i):
-    x, y = m(i*0.1 + start_lon, (i*0.1) * slope + start_lat)
+    x, y = m(i*1 + start_lon, (i*1) * slope + start_lat)
+    point.set_fontsize(12)
     point.set_position([x, y])
-    return point,
+    point2.set_fontsize(12)
+    x2, y2 = m(i+10, i+10)
+    point2.set_position([x2,y2])
+    return point,point2
 
 # call the animator.  blit=True means only re-draw the parts that have changed.
 anim = animation.FuncAnimation(plt.gcf(), animate, init_func=init,
-                               frames=100000, interval=1, blit=True)
+                               frames=int(abs(end_lon-start_lon)), interval=1, blit=True, repeat=False)
 
 plt.show()
